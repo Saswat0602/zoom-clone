46e0ef7d799c3d7a1cc8c741a83c72c4ccaf2d1b
Sawat <saswatranjan0602@gmail.com>
2024-03-31 19:58:42 +0530

    managing folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react'
+
+const RootLayout = ({children}:{children:ReactNode}) => {
+  return (
+    <main>
+        NavBar
+        {children}
+        Footer
+      
+    </main>
+  )
+}
+
+export default RootLayout
