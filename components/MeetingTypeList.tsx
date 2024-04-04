ee782abd5c9486378c78b2b3cfd31c029d7c7745
Sawat <saswatranjan0602@gmail.com>
2024-04-04 21:31:50 +0530

    added meeting button modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 import HomeCard from "./HomeCard";
 import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
 
 const MeetingTypeList = () => {
     const router = useRouter();
@@ -12,6 +13,11 @@ const MeetingTypeList = () => {
     const [meetingState, setMeetingState] = useState<
     'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
   >(undefined);
+
+  const createMeeting =()=>{
+    
+
+  }
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -42,7 +48,15 @@ const MeetingTypeList = () => {
         className="bg-yellow-1"
         handleClick={() => router.push("/recordings")}
       />
+      <MeetingModal 
+      isOpen={meetingState==='isInstantMeeting'}
+      onClose={()=>{setMeetingState(undefined)}}
+      className="text-center"
+      buttonText="Start Meeting"
+      handleClick={createMeeting}
+      />
     </section>
+    
   );
 };
 
