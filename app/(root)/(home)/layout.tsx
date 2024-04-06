6ed0598bf4e34d0839f1f0fecbffb99d11477eb1
Sawat <saswatranjan0602@gmail.com>
2024-04-06 23:08:58 +0530

    updated styles and icons added

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,7 +1,15 @@
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 import React, { ReactNode } from 'react'
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Zoom",
+  description: "Video Calling App",
+  icons:{
+    icon:'icons/logo.svg'
+  }
+};
 const HomeLayout = ({children}:{children:ReactNode}) => {
     return (
         <main className="relative">
