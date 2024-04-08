359b920456bebd48ee20e55185939dc77249aae5
Sawat <saswatranjan0602@gmail.com>
2024-04-08 22:49:00 +0530

    small push

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,17 +1,60 @@
 'use client';
 
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
+import MeetingCard from './MeetingCard';
 
 import Loader from './Loader';
 import { useGetCalls } from '@/hooks/useGetCalls';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-const CallList = ({type}:{type:'ended'|'upcoming' | 'recordings'}) => {
 
-    const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
+  const router = useRouter();
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } =
     useGetCalls();
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+
+
+  const getCalls = () => {
+    switch (type) {
+      case 'ended':
+        return endedCalls;
+      case 'recordings':
+        return recordings;
+      case 'upcoming':
+        return upcomingCalls;
+      default:
+        return [];
+    }
+  };
+  
+
+
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case 'ended':
+        return 'No Previous Calls';
+      case 'upcoming':
+        return 'No Upcoming Calls';
+      case 'recordings':
+        return 'No Recordings';
+      default:
+        return '';
+    }
+  };
+
+  useEffect(()=>{
+
+  }, [type, callRecordings]);
+
+
+
+
   return (
-    <div>CallList</div>
+    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+    <MeetingCard/>        
+    </div>
   )
 }
 
