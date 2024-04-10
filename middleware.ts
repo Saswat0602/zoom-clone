983a9429b35d377c6074c236e6ac1081f323c9e7
Sawat <saswatranjan0602@gmail.com>
2024-04-10 12:00:31 +0530

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
 
-const protectedRoutes = [
-    '/',
-  '/upcoming',
-  '/meeting(.*)',
-  '/previous',
-  '/recordings',
-  '/personal-room',
-  ];
-export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  // publicRoutes: ['/anyone-can-visit-this-route'],
-//   publicRoutes: protectedRoutes,
+export default authMiddleware({});
 
-
-});
- 
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
-  ]
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
